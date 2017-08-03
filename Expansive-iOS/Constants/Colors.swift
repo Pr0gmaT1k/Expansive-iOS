3f4686a81186eadc877e3e9917548cc040902466
julien.cloupet@niji.fr <julien.cloupet@niji.fr>
2017-08-03 09:25:12 +0200

    Setup SwiftGen

---
+++ Expansive-iOS/Constants/Colors.swift
@@ -0,0 +1,52 @@
+// Generated using SwiftGen, by O.Halligon — https://github.com/SwiftGen/SwiftGen
+
+#if os(iOS) || os(tvOS) || os(watchOS)
+  import UIKit.UIColor
+  typealias Color = UIColor
+#elseif os(OSX)
+  import AppKit.NSColor
+  typealias Color = NSColor
+#endif
+
+// swiftlint:disable operator_usage_whitespace
+extension Color {
+  convenience init(rgbaValue: UInt32) {
+    let red   = CGFloat((rgbaValue >> 24) & 0xff) / 255.0
+    let green = CGFloat((rgbaValue >> 16) & 0xff) / 255.0
+    let blue  = CGFloat((rgbaValue >>  8) & 0xff) / 255.0
+    let alpha = CGFloat((rgbaValue      ) & 0xff) / 255.0
+
+    self.init(red: red, green: green, blue: blue, alpha: alpha)
+  }
+}
+// swiftlint:enable operator_usage_whitespace
+
+// swiftlint:disable file_length
+// swiftlint:disable line_length
+
+// swiftlint:disable type_body_length
+enum ColorName {
+  /// <span style="display:block;width:3em;height:2em;border:1px solid black;background:#ffffff"></span>
+  /// Alpha: 100% <br/> (0xffffffff)
+  case test
+
+  var rgbaValue: UInt32 {
+    switch self {
+    case .test:
+      return 0xffffffff
+    }
+  }
+
+  var color: Color {
+    return Color(named: self)
+  }
+}
+// swiftlint:enable type_body_length
+
+extension Color {
+  convenience init(named name: ColorName) {
+    self.init(rgbaValue: name.rgbaValue)
+  }
+}
+// swiftlint:enable file_length
+// swiftlint:enable line_length
